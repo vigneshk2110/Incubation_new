4786a0802ab741251c608287100c809ac972f1fa
Administrator <Administrator@woodinc-51.csez.zohocorpin.com.>
2023-01-14 07:26:44 +0530

    New Commit

--- Vijay Sir Test/src/test1/HospitalManage.java
+++ Vijay Sir Test/src/test1/HospitalManage.java
@@ -17,7 +17,6 @@ public class HospitalManage {
 		super();
 	}
 
-
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);		
 		HospitalManage hm = new HospitalManage();
